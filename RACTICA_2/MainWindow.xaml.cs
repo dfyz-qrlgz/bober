06370da1564b3aef73d3ddc27d2609b5150ac79a
DFYZ <ivan6179217@mail.ru>
2023-04-02 23:46:15 +0300

    birds are gov drones

---
+++ RACTICA_2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RACTICA_2.libraryDataSetTableAdapters; 
+
+
+namespace RACTICA_2
+{
+    public partial class MainWindow : Window
+    {
+        authorsTableAdapter authors = new authorsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            datagrid.ItemsSource = authors.GetData();
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            second window = new second();
+            window.ShowDialog();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            authors.InsertQuery(Convert.ToInt32(Athid.Text), AthName.Text, AthMail.Text);
+            datagrid.ItemsSource = authors.GetData();
+        }
+    }
+}
---
+++ RACTICA_2/second.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RACTICA_2.libraryDataSetTableAdapters;
+
+namespace RACTICA_2
+{
+    
+    public partial class second : Window
+    {
+        booksTableAdapter books = new booksTableAdapter();
+        public second()
+        {
+            InitializeComponent();
+            datagrid2.ItemsSource = books.GetData();
+        }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            books.InsertQuery(Convert.ToInt32(Bookid.Text), BkName.Text, Convert.ToInt32(BAthid.Text), PubDate.Text);
+            datagrid2.ItemsSource = books.GetData();
+        }
+    }
+}
